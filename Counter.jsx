c810801ca6cfe90d8daf3a07e391d4be4f855bac
Pb9513git <110809447+Pb9513git@users.noreply.github.com>
2025-07-22 17:02:00 +0530

    Add files via upload

---
+++ Counter.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
